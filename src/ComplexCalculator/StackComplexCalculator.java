68a599f62d38c02f53c6d06a074f1b254f201a60
P11etro <pietro.pantani.03@gmail.com>
2023-12-08 15:17:20 +0100

    Added icon and application name

--- src/ComplexCalculator/StackComplexCalculator.java
+++ src/ComplexCalculator/StackComplexCalculator.java
@@ -5,6 +5,7 @@ import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -19,6 +20,9 @@ public class StackComplexCalculator extends Application {
         
         Scene scene = new Scene(root);
         stage.setResizable(false);
+        
+        stage.setTitle("Scientific Calculator Beta");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
 
         stage.setScene(scene);
         stage.show();
