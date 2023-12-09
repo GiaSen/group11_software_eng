66e55e4ce3165dd32776362b39eadeeae14d824f
thatrsux <vincenzom121@gmail.com>
2023-12-09 16:40:30 +0100

    Operation class description

--- src/ComplexCalculatorOperation/Operation.java
+++ src/ComplexCalculatorOperation/Operation.java
@@ -4,6 +4,9 @@ import ComplexCalculator.Complex;
 import java.util.ArrayDeque;
 
 /**
+ * The Operation class abstracts the concept of operation providing the basics
+ * methods to handle different types of operations on the calculator such as
+ * ArithmeticOperation, StackOperation or VariableOperation.
  *
  * @author group11
  */
