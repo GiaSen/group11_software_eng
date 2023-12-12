f8b730cb2bf930f82d4d64d1ded8532b428503a8
giasen <giammasena@gmail.com>
2023-12-12 15:44:41 +0100

    ComplexTest: fix typo
     format too

--- src/test/java/ComplexCalculator/ComplexTest.java
+++ src/test/java/ComplexCalculator/ComplexTest.java
@@ -9,22 +9,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ComplexTest {
-    
+
     public ComplexTest() {
     }
-    
+
     @BeforeAll
     public static void setUpClass() {
     }
-    
+
     @AfterAll
     public static void tearDownClass() {
     }
-    
+
     @BeforeEach
     public void setUp() {
     }
-    
+
     @AfterEach
     public void tearDown() {
     }
@@ -38,31 +38,34 @@ public class ComplexTest {
         Complex instance = new Complex("5.002+41j");
         double expResult = 5.002;
         double result = instance.getReal();
-        assertEquals(expResult,result,0.0);
+        assertEquals(expResult, result, 0.0);
     }
+
     @Test
     public void testGetReal2() {
         System.out.println("getReal");
         Complex instance = new Complex("17j");
         double expResult = 0.0;
         double result = instance.getReal();
-        assertEquals(expResult,result,0.0);
+        assertEquals(expResult, result, 0.0);
     }
+
     @Test
     public void testGetReal3() {
         System.out.println("getReal");
         Complex instance = new Complex("-1.324+41j");
         double expResult = -1.324;
         double result = instance.getReal();
-        assertEquals(expResult,result,0.0);
+        assertEquals(expResult, result, 0.0);
     }
+
     @Test
     public void testGetReal4() {
         System.out.println("getReal");
         Complex instance = new Complex("-3.54j");
         double expResult = 0.0;
         double result = instance.getReal();
-        assertEquals(expResult,result,0.0);
+        assertEquals(expResult, result, 0.0);
     }
 
     /**
@@ -76,6 +79,7 @@ public class ComplexTest {
         double result = instance.getImaginary();
         assertEquals(expResult, result, 0.0);
     }
+
     @Test
     public void testGetImaginary2() {
         System.out.println("getImaginary");
@@ -84,6 +88,7 @@ public class ComplexTest {
         double result = instance.getImaginary();
         assertEquals(expResult, result, 0.0);
     }
+
     @Test
     public void testGetImaginary3() {
         System.out.println("getImaginary");
@@ -92,6 +97,7 @@ public class ComplexTest {
         double result = instance.getImaginary();
         assertEquals(expResult, result, 0.0);
     }
+
     @Test
     public void testGetImaginary4() {
         System.out.println("getImaginary");
@@ -111,8 +117,9 @@ public class ComplexTest {
         Complex expResult = new Complex("-2.551+43j");
         Complex result = instance.negate();
         assertEquals(expResult, result);
-        
+
     }
+
     @Test
     public void testNegate2() {
         System.out.println("negate");
@@ -121,6 +128,7 @@ public class ComplexTest {
         Complex result = instance.negate();
         assertEquals(expResult, result);
     }
+
     @Test
     public void testNegate3() {
         System.out.println("negate");
@@ -129,6 +137,7 @@ public class ComplexTest {
         Complex result = instance.negate();
         assertEquals(expResult, result);
     }
+
     @Test
     public void testNegate4() {
         System.out.println("negate");
@@ -150,6 +159,7 @@ public class ComplexTest {
         Complex result = instance.sum(n);
         assertEquals(expResult, result);
     }
+
     @Test
     public void testSum2() {
         System.out.println("sum");
@@ -159,6 +169,7 @@ public class ComplexTest {
         Complex result = instance.sum(n);
         assertEquals(expResult, result);
     }
+
     @Test
     public void testSum3() {
         System.out.println("sum");
@@ -168,6 +179,7 @@ public class ComplexTest {
         Complex result = instance.sum(n);
         assertEquals(expResult, result);
     }
+
     @Test
     public void testSum4() {
         System.out.println("sum");
@@ -190,6 +202,7 @@ public class ComplexTest {
         Complex result = instance.sub(n);
         assertEquals(expResult, result);
     }
+
     @Test
     public void testSub2() {
         System.out.println("sub");
@@ -199,6 +212,7 @@ public class ComplexTest {
         Complex result = instance.sub(n);
         assertEquals(expResult, result);
     }
+
     @Test
     public void testSub3() {
         System.out.println("sub");
@@ -208,6 +222,7 @@ public class ComplexTest {
         Complex result = instance.sub(n);
         assertEquals(expResult, result);
     }
+
     @Test
     public void testSub4() {
         System.out.println("sub");
@@ -228,9 +243,10 @@ public class ComplexTest {
         Complex instance = new Complex("24.4+3.66j");
         Complex expResult = new Complex("0");
         Complex result = instance.product(n);
-        assertEquals(expResult.getReal(), result.getReal(),0.00001);
-        assertEquals(expResult.getImaginary(),result.getImaginary(),0.00001);
+        assertEquals(expResult.getReal(), result.getReal(), 0.00001);
+        assertEquals(expResult.getImaginary(), result.getImaginary(), 0.00001);
     }
+
     @Test
     public void testProduct2() {
         System.out.println("product");
@@ -238,9 +254,10 @@ public class ComplexTest {
         Complex instance = new Complex("j");
         Complex expResult = new Complex("-1");
         Complex result = instance.product(n);
-        assertEquals(expResult.getReal(), result.getReal(),0.00001);
-        assertEquals(expResult.getImaginary(),result.getImaginary(),0.00001);
+        assertEquals(expResult.getReal(), result.getReal(), 0.00001);
+        assertEquals(expResult.getImaginary(), result.getImaginary(), 0.00001);
     }
+
     @Test
     public void testProduct3() {
         System.out.println("product");
@@ -248,9 +265,10 @@ public class ComplexTest {
         Complex instance = new Complex("3");
         Complex expResult = new Complex("21.63+18j");
         Complex result = instance.product(n);
-        assertEquals(expResult.getReal(), result.getReal(),0.00001);
-        assertEquals(expResult.getImaginary(),result.getImaginary(),0.00001);
+        assertEquals(expResult.getReal(), result.getReal(), 0.00001);
+        assertEquals(expResult.getImaginary(), result.getImaginary(), 0.00001);
     }
+
     @Test
     public void testProduct4() {
         System.out.println("product");
@@ -258,8 +276,8 @@ public class ComplexTest {
         Complex instance = new Complex("7.16+2.51j");
         Complex expResult = new Complex("-2.127+29.002j");
         Complex result = instance.product(n);
-        assertEquals(expResult.getReal(), result.getReal(),0.00001);
-        assertEquals(expResult.getImaginary(),result.getImaginary(),0.00001);
+        assertEquals(expResult.getReal(), result.getReal(), 0.00001);
+        assertEquals(expResult.getImaginary(), result.getImaginary(), 0.00001);
     }
 
     /**
@@ -272,9 +290,10 @@ public class ComplexTest {
         Complex instance = new Complex("1");
         Complex expResult = new Complex("-1j");
         Complex result = instance.division(n);
-        assertEquals(expResult.getReal(), result.getReal(),0.00001);
-        assertEquals(expResult.getImaginary(),result.getImaginary(),0.00001);
+        assertEquals(expResult.getReal(), result.getReal(), 0.00001);
+        assertEquals(expResult.getImaginary(), result.getImaginary(), 0.00001);
     }
+
     @Test
     public void testDivision2() {
         System.out.println("division");
@@ -282,9 +301,10 @@ public class ComplexTest {
         Complex instance = new Complex("2.3j");
         Complex expResult = new Complex("0.22439+0.28049j");
         Complex result = instance.division(n);
-        assertEquals(expResult.getReal(), result.getReal(),0.00001);
-        assertEquals(expResult.getImaginary(),result.getImaginary(),0.00001);
+        assertEquals(expResult.getReal(), result.getReal(), 0.00001);
+        assertEquals(expResult.getImaginary(), result.getImaginary(), 0.00001);
     }
+
     @Test
     public void testDivision3() {
         System.out.println("division");
@@ -292,15 +312,16 @@ public class ComplexTest {
         Complex instance = new Complex("-42.4+j");
         Complex expResult = new Complex("-0.25-0.95j");
         Complex result = instance.division(n);
-        assertEquals(expResult.getReal(), result.getReal(),0.00001);
-        assertEquals(expResult.getImaginary(),result.getImaginary(),0.00001);
+        assertEquals(expResult.getReal(), result.getReal(), 0.00001);
+        assertEquals(expResult.getImaginary(), result.getImaginary(), 0.00001);
     }
+
     @Test
     public void testDivision4() {
         System.out.println("division");
         Complex n = new Complex("0");
         Complex instance = new Complex("3.98+0.5j");
-        assertThrows(ZeroDivisionException.class,() -> {
+        assertThrows(ZeroDivisionException.class, () -> {
             instance.division(n);
         });
     }
@@ -314,41 +335,43 @@ public class ComplexTest {
         Complex instance = new Complex("-1");
         Complex expResult = new Complex("j");
         Complex result = instance.sqrt();
-        assertEquals(expResult.getReal(), result.getReal(),0.00001);
-        assertEquals(expResult.getImaginary(),result.getImaginary(),0.00001);
+        assertEquals(expResult.getReal(), result.getReal(), 0.00001);
+        assertEquals(expResult.getImaginary(), result.getImaginary(), 0.00001);
     }
+
     @Test
     public void testSqrt2() {
         System.out.println("sqrt");
         Complex instance = new Complex("4+3j");
         Complex expResult = new Complex("2.12132+0.70711j");
         Complex result = instance.sqrt();
-        assertEquals(expResult.getReal(), result.getReal(),0.00001);
-        assertEquals(expResult.getImaginary(),result.getImaginary(),0.00001);
+        assertEquals(expResult.getReal(), result.getReal(), 0.00001);
+        assertEquals(expResult.getImaginary(), result.getImaginary(), 0.00001);
     }
+
     @Test
     public void testSqrt3() {
         System.out.println("sqrt");
         Complex instance = new Complex("4.16");
         Complex expResult = new Complex("2.03961");
         Complex result = instance.sqrt();
-        assertEquals(expResult.getReal(), result.getReal(),0.00001);
-        assertEquals(expResult.getImaginary(),result.getImaginary(),0.00001);
+        assertEquals(expResult.getReal(), result.getReal(), 0.00001);
+        assertEquals(expResult.getImaginary(), result.getImaginary(), 0.00001);
     }
+
     @Test
     public void testSqrt4() {
         System.out.println("sqrt");
         Complex instance = new Complex("-1-3j");
         Complex expResult = new Complex("1.03978-1.44262j");
         Complex result = instance.sqrt();
-        assertEquals(expResult.getReal(), result.getReal(),0.00001);
-        assertEquals(expResult.getImaginary(),result.getImaginary(),0.00001);
+        assertEquals(expResult.getReal(), result.getReal(), 0.00001);
+        assertEquals(expResult.getImaginary(), result.getImaginary(), 0.00001);
     }
 
     /**
      * Test of isComplex method, of class Complex.
      */
-    
     @Test
     public void testIsComplex1() {
         System.out.println("isComplex");
@@ -357,6 +380,7 @@ public class ComplexTest {
         boolean result = Complex.isComplex(s);
         assertEquals(expResult, result);
     }
+
     @Test
     public void testIsComplex2() {
         System.out.println("isComplex");
@@ -365,6 +389,7 @@ public class ComplexTest {
         boolean result = Complex.isComplex(s);
         assertEquals(expResult, result);
     }
+
     @Test
     public void testIsComplex3() {
         System.out.println("isComplex");
@@ -373,6 +398,7 @@ public class ComplexTest {
         boolean result = Complex.isComplex(s);
         assertEquals(expResult, result);
     }
+
     @Test
     public void testIsComplex4() {
         System.out.println("isComplex");
@@ -393,29 +419,32 @@ public class ComplexTest {
         String result = instance.toString();
         assertEquals(expResult, result);
     }
+
     @Test
     public void testToString2() {
         System.out.println("toString");
         Complex instance = new Complex(".5+3j");
-        String expResult = "0,5+3j";
+        String expResult = "0.5+3j";
         String result = instance.toString();
         assertEquals(expResult, result);
     }
+
     @Test
     public void testToString3() {
         System.out.println("toString");
         Complex instance = new Complex("4.21+1j");
-        String expResult = "4,21+j";
+        String expResult = "4.21+j";
         String result = instance.toString();
         assertEquals(expResult, result);
     }
+
     @Test
     public void testToString4() {
         System.out.println("toString");
         Complex instance = new Complex("0+3.2j");
-        String expResult = "3,2j";
+        String expResult = "3.2j";
         String result = instance.toString();
         assertEquals(expResult, result);
     }
-    
+
 }
