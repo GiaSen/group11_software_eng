58e7353afb1a8b24dd180867f37bf249aaf8694b
thatrsux <vincenzom121@gmail.com>
2023-12-12 17:12:24 +0100

    Test: Calculator: added StackLimitTest

--- src/test/java/ComplexCalculatorOperation/CalculatorTest.java
+++ src/test/java/ComplexCalculatorOperation/CalculatorTest.java
@@ -20,24 +20,25 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author vince
  */
 public class CalculatorTest {
+
     Calculator instance;
-    
+
     public CalculatorTest() {
     }
-    
+
     @BeforeAll
     public static void setUpClass() {
     }
-    
+
     @AfterAll
     public static void tearDownClass() {
     }
-    
+
     @BeforeEach
     public void setUp() {
         instance = new Calculator();
     }
-    
+
     @AfterEach
     public void tearDown() {
     }
@@ -48,20 +49,20 @@ public class CalculatorTest {
     @Test
     public void testInterpreter() {
         System.out.println("interpreter");
-                
+
         instance.interpreter("3");
-        
-        assertEquals(1,instance.getStack().size());
+
+        assertEquals(1, instance.getStack().size());
     }
-    
+
     @Test
     public void testInterpreter2() {
         System.out.println("interpreter wrong input");
-        
+
         String str = "test"; //example of a bad input insertion
-        
-        assertThrows(InvalidInputException.class,()->instance.interpreter(str));
-        
+
+        assertThrows(InvalidInputException.class, () -> instance.interpreter(str));
+
     }
 
     /**
@@ -71,28 +72,28 @@ public class CalculatorTest {
     public void testGetStack() {
         System.out.println("getStack when empty");
         ArrayDeque<Complex> stack = instance.getStack();
-        
+
         assertNotNull(stack);
         assertTrue(stack.isEmpty());
-        
+
     }
-    
+
     @Test
     public void testGetStack2() {
         System.out.println("getStack when not empty");
         ArrayDeque<Complex> stack = instance.getStack();
-        
+
         assertNotNull(stack);
         assertTrue(stack.isEmpty());
-        
-        Complex n1 = new Complex(1,4);
-        Complex n2 = new Complex(-2,-8.9);
+
+        Complex n1 = new Complex(1, 4);
+        Complex n2 = new Complex(-2, -8.9);
         stack.push(n1);
         stack.push(n2);
-        
+
         assertFalse(stack.isEmpty());
-        assertEquals(2,instance.getStack().size());
-        
+        assertEquals(2, instance.getStack().size());
+
     }
 
     /**
@@ -102,26 +103,34 @@ public class CalculatorTest {
     public void testGetMap() {
         System.out.println("getMap when empty");
         HashMap<String, Complex> map = instance.getMap();
-        
+
         assertNotNull(map);
         assertTrue(map.isEmpty());
     }
-    
+
     @Test
     public void testGetMap2() {
         System.out.println("getMap when not empty");
         HashMap<String, Complex> map = instance.getMap();
-        
+
         assertNotNull(map);
         assertTrue(map.isEmpty());
-        
-        Complex n1 = new Complex(10,8);
-        Complex n2 = new Complex(-1,7.2);
-        map.put("a",n1);
-        map.put("b",n2);
-        
+
+        Complex n1 = new Complex(10, 8);
+        Complex n2 = new Complex(-1, 7.2);
+        map.put("a", n1);
+        map.put("b", n2);
+
         assertFalse(map.isEmpty());
-        assertEquals(2,instance.getMap().size());
+        assertEquals(2, instance.getMap().size());
     }
-    
+
+    @Test
+    public void testStackLimit(){
+        Integer i;
+        for(i=0;i<1000000;i++){ //checks if the stack supports 1_000_000 elements
+            instance.interpreter(i.toString()+ "+" + i.toString()+"j");
+        }
+        assertEquals(i,instance.getStack().size());
+    }
 }
